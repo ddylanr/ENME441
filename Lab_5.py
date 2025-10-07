cf1111710ea23a02e64541b3ee3a4830032ff202
ddylanr <130129906+ddylanr@users.noreply.github.com>
2025-10-06 23:28:56 -0400

    Add files via upload

--- Lab_5.py
+++ Lab_5.py
@@ -3,28 +3,30 @@ from time import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-p17 = 17 # pin 17
-p27 = 27 # pin 27
+p1 = 17 # pin 17
+p2 = 27 # pin 27
 t0 = time()
 f = 0.2 # frequency in Hz
-GPIO.setup(p17, GPIO.OUT)
-GPIO.setup(p27, GPIO.OUT)
+GPIO.setup(p1, GPIO.OUT)
+GPIO.setup(p2, GPIO.OUT)
 
 
-pwm = GPIO.PWM(p17, 500) # create PWM object @ 500 Hz
-PWM = GPIO.PWM(p27, 500) # create PWM object @ 500 Hz
+pwm1 = GPIO.PWM(p1, 500) # create PWM object @ 500 Hz
+pwm2 = GPIO.PWM(p2, 500) # create PWM object @ 500 Hz
 try:
-    pwm.start(0) # initiate PWM at 0% duty cycle
+    pwm1.start(0) # initiate PWM at 0% duty cycle
+    pwm2.start(0) # initiate PWM at 0% duty cycle
     while 1:
         t = time() - t0
         B = (sin(2*pi*0.2*t))**2
         b = (sin(2*pi*f*t - (pi/11)))**2
         dc1 = B * 100 # scale to 0-100
         dc2 = b * 100 # scale to 0-100
-        pwm.ChangeDutyCycle(dc1) # set duty cycle
-        PWM.ChangeDutyCycle(dc2) # set duty cycle
+        pwm1.ChangeDutyCycle(dc1) # set duty cycle
+        pwm2.ChangeDutyCycle(dc2) # set duty cycle
 except KeyboardInterrupt: # stop gracefully on ctrl-C
     print('\nExiting')
 
-pwm.stop()
+pwm1.stop()
+pwm2.stop()
 GPIO.cleanup()
