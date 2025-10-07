019bdda410588d6549f3660764568ada56315bd4
ddylanr <130129906+ddylanr@users.noreply.github.com>
2025-10-06 23:59:52 -0400

    Add files via upload

--- Lab_5.py
+++ Lab_5.py
@@ -3,30 +3,29 @@ from time import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-p1 = 17 # pin 17
-p2 = 27 # pin 27
+pin_outs = [2, 3, 4, 17, 27, 22, 10, 9, 11, 5]
 t0 = time()
 f = 0.2 # frequency in Hz
-GPIO.setup(p1, GPIO.OUT)
-GPIO.setup(p2, GPIO.OUT)
+step = pi/11 # phase step
 
+pins = []
+for pin in pin_outs:
+    GPIO.setup(pin, GPIO.OUT)
+    pwm = GPIO.PWM(pin, 500) # create PWM object @ 500 Hz
+    pwm.start(0) # start PWM with 0% duty cycle
+    pins.append(pwm)
 
-pwm1 = GPIO.PWM(p1, 500) # create PWM object @ 500 Hz
-pwm2 = GPIO.PWM(p2, 500) # create PWM object @ 500 Hz
 try:
-    pwm1.start(0) # initiate PWM at 0% duty cycle
-    pwm2.start(0) # initiate PWM at 0% duty cycle
     while 1:
         t = time() - t0
-        B = (sin(2*pi*0.2*t))**2
-        b = (sin(2*pi*f*t - (pi/11)))**2
-        dc1 = B * 100 # scale to 0-100
-        dc2 = b * 100 # scale to 0-100
-        pwm1.ChangeDutyCycle(dc1) # set duty cycle
-        pwm2.ChangeDutyCycle(dc2) # set duty cycle
+        for i in range(len(pins)):
+            # calculate duty cycle
+            B = (sin(2*pi*0.2*t - i*(pi/11)))**2  # Removed '- (p)' since 'p' is undefined
+            dc = B * 100 # scale to 0-100
+            pwm.ChangeDutyCycle(dc) # set duty cycle        
+            
 except KeyboardInterrupt: # stop gracefully on ctrl-C
     print('\nExiting')
 
-pwm1.stop()
-pwm2.stop()
+pwm.stop()
 GPIO.cleanup()
