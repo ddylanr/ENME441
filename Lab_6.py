609d809e9ec04ead067236386348e286afab162b
ddylanr <130129906+ddylanr@users.noreply.github.com>
2025-10-19 22:51:53 -0400

    Fix import statement for Bug class

--- Lab_6.py
+++ Lab_6.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 from shifter import Shifter
-from bug import Bug
+from shifter import Bug
 
 s = Shifter(23, 24, 25)                # Initialize shift register
 b = Bug(s, timestep=0.05, x=3, isWrapOn=False)  # Create Bug object
@@ -10,3 +10,4 @@ try:
 except KeyboardInterrupt:
     b.stop()
     GPIO.cleanup()
+
